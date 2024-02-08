1676f7e6b8eba17685e6e292d114abc1ae781393
halah.alshaikhly@gmail.com <halah.alshaikhly@gmail.com>
2024-02-07 21:00:37 -0800

    initial commit

---
+++ backend/index.ts
@@ -0,0 +1,19 @@
+import  express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+
+app.get("/api", (req: Request, res: Response) => {
+res.json({ message: "Hello World! I'm JSON" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
