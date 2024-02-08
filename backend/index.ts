4a817d8d2d27843bdf79d3f0cedca1b59720dc12
halah.alshaikhly@gmail.com <halah.alshaikhly@gmail.com>
2024-02-08 13:30:45 -0800

    configures dotenv

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,8 @@
 import  express, { Request, Response } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3001;
