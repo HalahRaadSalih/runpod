e6795d6ebb4fa26fdae57375983d5c5b6d2b3ae7
halah.alshaikhly@gmail.com <halah.alshaikhly@gmail.com>
2024-02-09 23:42:58 -0800

    [front-end] configures wrappers for app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,24 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { GenerateImages } from './pages/GenerateImages/GenerateImages.tsx';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+
+// const queryClient = new QueryClient();
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <GenerateImages />,
+    },
+  ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    // <QueryClientProvider client={queryClient}>
+        <StyledEngineProvider injectFirst>
+            <CssBaseline />
+            <RouterProvider router={router} />
+        </StyledEngineProvider>
+    // </QueryClientProvider>
+
 )
