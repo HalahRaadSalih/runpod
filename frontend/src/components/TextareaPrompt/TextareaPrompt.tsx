68bf32935550239aa5b7ae68cf2b78ebde370e52
halah.alshaikhly@gmail.com <halah.alshaikhly@gmail.com>
2024-02-09 23:44:38 -0800

    [front-end] adds <TextareaPrompt />

---
+++ frontend/src/components/TextareaPrompt/TextareaPrompt.tsx
@@ -0,0 +1,37 @@
+import { Box } from "@mui/material";
+import { Textarea } from "@mui/joy";
+
+
+interface TextareaPromptProps {
+    placeholder: string;
+    name: string;
+    required?: boolean;
+    minRows?: number;
+}
+
+export const TextareaPrompt = (props: TextareaPromptProps) => {
+    const { placeholder, name, required = false, minRows } = props;
+    return (
+        <Box>
+                <Textarea
+                    name={name}
+                    placeholder={placeholder}
+                    required={required}
+                    minRows={minRows || 2}
+                    sx={{
+                        mb: 1 ,
+                        '--Textarea-focusedInset': 'var(--any, )',
+                        '--Textarea-focusedThickness': '0.25rem',
+                        '--Textarea-focusedHighlight': 'rgba(13,110,253,.25)',
+                        '&::before': {
+                        transition: 'box-shadow .15s ease-in-out',
+                        },
+                        '&:focus-within': {
+                        borderColor: '#86b7fe',
+                        },
+                    }}
+                />
+        </Box>
+
+        );
+};
---
+++ frontend/src/components/TextareaPrompt/index.ts
@@ -0,0 +1,1 @@
+export { TextareaPrompt } from './TextareaPrompt';
