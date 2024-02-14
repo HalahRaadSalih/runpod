735c3723ffba964a992fddd3a6f387ae5f92aa87
halah.alshaikhly@gmail.com <halah.alshaikhly@gmail.com>
2024-02-13 18:16:02 -0800

    [front-end] installs test framework vitest + react-testing library

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
+// 'vite?-package' should be listed in the project's dependencies, not devDependencies
+
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    setupFiles: './src/test/setupTests.ts',
+    include: ['src/**/*.test.ts?(x)'],
+    cache: false,
+    silent: true
+  }
 })
